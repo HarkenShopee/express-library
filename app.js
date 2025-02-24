b463b4d41eb0766ae10672fb57065ba6a80f6bb9
Harken <haikun.xiu@npt.sg>
2025-02-24 16:02:40 +0800

    feat: 🎸 initial project

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
+// all express modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// create express app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// use morgan 'dev' type for log
 app.use(logger('dev'));
+// use json for express body
 app.use(express.json());
+// url encode
 app.use(express.urlencoded({ extended: false }));
+// cookie parse
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// static public folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// page routers
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.send('you are sooooooooooooooo cool!');
+});
+
 module.exports = router;
